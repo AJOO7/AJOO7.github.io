914420fe4995b03eb80456ed37d7abbc661371f5
AJ00007 <67185443+AJ00007@users.noreply.github.com>
2020-12-19 01:12:11 +0530

    adding typewriting animation

--- index.js
+++ index.js
@@ -66,6 +66,22 @@ function checkScroll() {
 
     }
 }
+// var i = 0;
+// var txt = 'HI';
+// var speed = 50;
+
+// function typeWriter() {
+//     console.log("hello")
+//     if (i < txt.length) {
+//         $("coverName").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+
+// if (i == 0) {
+//     typeWriter();
+// }
 
 
 
